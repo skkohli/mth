1d33df647aadf96def45db89e7e073047e79ea20
skkohli <kohli.iitm@gmail.com>
2026-07-07 16:38:10 +0530

    UI theme modified, ui bugs resolved, cart icon hidden

--- wp-content/themes/listeo-child/functions.php
+++ wp-content/themes/listeo-child/functions.php
@@ -2,7 +2,20 @@
 add_action( 'wp_enqueue_scripts', 'listeo_enqueue_styles' );
 function listeo_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css',array('bootstrap','font-awesome-5','font-awesome-5-shims','simple-line-icons','listeo-woocommerce') );
+}
 
+add_action( 'wp_enqueue_scripts', 'mth_enqueue_child_theme_overrides', 99 );
+function mth_enqueue_child_theme_overrides() {
+    $dependencies = wp_style_is( 'listeo-style', 'registered' ) || wp_style_is( 'listeo-style', 'enqueued' )
+        ? array( 'listeo-style' )
+        : array( 'parent-style' );
+
+    wp_enqueue_style(
+        'mth-child-theme-overrides',
+        get_stylesheet_directory_uri() . '/style.css',
+        $dependencies,
+        filemtime( get_stylesheet_directory() . '/style.css' )
+    );
 }
 
 
